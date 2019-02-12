24c75058446c0ef8d34a28899e328bd9dd658107
Jared Giangrasso <jagiangrasso@gmail.com>
2019-02-12 09:41:53 -0500

    configure redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import WeekContainer from './WeekContainer.js'
+import { Provider } from 'react-redux';
+import { store } from './actions.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <WeekContainer />
-      </div>
+      <Provider store={ store }>
+	      <div className="App">
+	        <WeekContainer />
+	      </div>
+	  </Provider>
     );
   }
 }
